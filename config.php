411c1c853ea5c888cb0476f6e40fe22c61553d17
张强 <1533696518@qq.com>
2016-02-03 21:25:48 +0800

    update

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 #程序配置信息
-define("S_HOST","127.0.0.1");
-define("S_PORT",3306);
-define("S_USER","root");
-define("S_PWD","19930526");
+define("S_HOST","");
+define("S_PORT",);
+define("S_USER","");
+define("S_PWD","");
 ?>
